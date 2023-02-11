7b6d77204f43c11e87483f2ec51240bc30e0467c
Daniel Gapso <dandan11110000@gmail.com>
2023-02-11 18:20:30 +0200

    ⚒️

--- js/js.js
+++ js/js.js
@@ -2,7 +2,7 @@ const coinUrl = "https://api.coingecko.com/api/v3/coins/list";
 const infoUrl = "https://api.coingecko.com/api/v3/coins/{id}";
 
 let allCoins = [];
-let allCoinsInfo=[];
+let allCoinsInfo = [];
 
 $(document).ready(function () {
   $.get(coinUrl).done(function (data) {
@@ -11,12 +11,11 @@ $(document).ready(function () {
   });
 });
 
-
 const getData = () => {
   $("#container").html("");
   for (let counter = 0; counter < 100; counter++) {
     $("#container").append(`
-    <div class="Box" id="box-${counter}-${allCoins[counter].id}">
+    <div class="Box" id="box-${allCoins[counter].id}">
     <div class="card" style="width: 18rem">
       <div class="card-body">
         <div class="form-check form-switch" id="addSwitch">
@@ -26,37 +25,33 @@ const getData = () => {
         <h5 class="card-title">${allCoins[counter].symbol}</h5>
         <p class="card-text">${allCoins[counter].id}</p>
         <button class="btn btn-primary" type="button" data-bs-toggle="collapse" 
-        data-bs-target="#collapseExample-${counter}-${allCoins[counter].id}"
-        onclick="getCoinInfo('${allCoins[counter].id}', '#collapseExample-${counter}-${allCoins[counter].id}')" 
+        data-bs-target="#collapseExample-${allCoins[counter].id}"
+        onclick="getCoinInfo('${allCoins[counter].id}', '#collapseExample-${allCoins[counter].id}')" 
         aria-expanded="false"
-        aria-controls="collapseExample-${counter}-${allCoins[counter].id}">
+        aria-controls="collapseExample-${allCoins[counter].id}">
           more info
         </button>
-        <div class="collapse" id="collapseExample-${counter}-${allCoins[counter].id}">
+        <div class="collapse" id="collapseExample-${allCoins[counter].id}">
           <div class="card card-body">
             <div id="coinInfo-${allCoins[counter].id}"></div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
   `);
-  }};
+  }
+};
 
-  const getCoinInfo = (id, target) => {
-    const url = infoUrl.replace("{id}", id);
-    $.get(url, function(data) {
-      allCoinsInfo = data;
-      console.log("data: ", data);
-      printinfo(id, data);
-      $(target).collapse('show');
-    });
-  };
-  
-  const printinfo = (id, data) => {
-    $("#coinInfo-" + id).append(`
-    <div class="collapse" id="collapseExample">
-      <div class="card card-body">
+const getCoinInfo = (id) => {
+  const url = infoUrl.replace("{id}", id);
+  $.get(url, function (data) {
+    allCoinsInfo = data;
+    printinfo(id, data);
+  });
+};
+
+const printinfo = (id, data) => {
+ 
+  $("#coinInfo-" + id).append(`
         <div>
           <img src=${data.image.thumb} width=100/><br/>
           <h5 class="card-text">coin name ${data.name}</h5>
@@ -75,6 +70,4 @@ const getData = () => {
       </div>
     </div>
     `);
-  };
-
-   
+};
